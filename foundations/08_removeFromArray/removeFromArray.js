6e71a76ec50526e301f434b84cdca4fa989a8444
Vic <123456wangtom@gmail.com>
2026-02-28 19:30:46 +1300

    finished exercises

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (arr, ...manyMoreArgs) {
+    //two loop
+    //outer loop  Iterate each target item
+    //inner loop iterate full array, find all the possble postion and remove it
+
+    for (let i = 0; i < manyMoreArgs.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] === manyMoreArgs[i])
+                delete arr[j];
+        }
+
+    }
+
+    return arr.filter(item => item !== undefined );
+
+}
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (!(Number.isInteger(num1) && num1 > 0) || !(Number.isInteger(num2) && num2 > 0))
+        return 'ERROR';
+
+    let sum = 0;
+    if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+    }
+
+    if (num2 < num1) {
+        for (let i = num2; i <= num1; i++) {
+            sum += i;
+        }
+    }
+
+    return sum;
+
+
+
+
+
+
 
 };
 
